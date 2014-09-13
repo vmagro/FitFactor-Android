3ef47bb63595816336ee0d8b6e2630342081ed71
sciencepro <dcarr@scienceprousa.com>
2014-09-12 22:44:06 -0400

    repush some stuff

--- mobile/src/main/java/io/perihelion/fitfactor/MainActivity.java
+++ mobile/src/main/java/io/perihelion/fitfactor/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.parse.Parse;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.initialize(this, "KqI3fIwrmgp1rep6UX31wZipcJACRJwtG66GNYoV", "fwXo7e2YQogv0OQybIhJqYHmsIVmEWZWNI92nbg0");
     }
 
 
@@ -28,9 +29,12 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
--- wear/src/main/java/io/perihelion/fitfactor/MainWearActivity.java
+++ wear/src/main/java/io/perihelion/fitfactor/MainWearActivity.java
@@ -1,24 +1,70 @@
 package io.perihelion.fitfactor;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.wearable.view.CircledImageView;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MainWearActivity extends Activity {
+public class MainWearActivity extends Activity implements SensorEventListener {
 
+    private CircledImageView mCircledImageView;
     private TextView mTextView;
+
+    Sensor mHeartRateSensor;
+    SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_wear);
+
+        mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+
+        //View
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mCircledImageView = (CircledImageView) stub.findViewById(R.id.circle);
+                mTextView = (TextView) stub.findViewById(R.id.value);
             }
         });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mSensorManager != null){
+            mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensorManager!=null)
+            mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            if ((int)event.values[0]>0) {
+                mCircledImageView.setCircleColor(getResources().getColor(R.color.green));
+                mTextView.setText("" + (int) event.values[0]);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
